0544fc22ef8c9c65c909500c50c20c746719995a
kai <kth6rtuo8@gmail.com>
2019-01-28 00:45:55 +0900

    パスの修正”

--- mysite/settings.py
+++ mysite/settings.py
@@ -71,10 +71,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql',  
+        'USER': 'root',  
+        'PASSWORD': 'hogehoge',  
+        'HOST': 'fugafuga', 
+        'PORT': '3306', 
     }
 }
 
@@ -133,7 +144,7 @@ ROOT_PATH = os.path.dirname(__file__)
 
 # 絶対Path参照は今後修正要
 STATICFILES_DIRS = [
-    'C:/Users/Kai Kodama/djangogirls/static'
+    'C:/Users/hogehoge/fugafuga/static'
 ]
 
 try:
